21299f8563927a5d7ba3cb3da07e999caa6ebbf5
Aaron Liu <aaronliunj@gmail.com>
2022-06-09 10:47:27 -0500

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,17 +5,17 @@ const siteMetadata = {
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://aaron-liu.vercel.app/',
+  siteRepo: 'https://github.com/websiteguy1222/my-app',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
+  email: 'aaronliuwi@gmail.com',
+  github: 'https://github.com/websiteguy1222',
+  twitter: 'https://twitter.com/ClubElmbrook',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/in/aaron-liu-4768091ba/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
